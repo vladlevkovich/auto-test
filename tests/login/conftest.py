d98648f07b15bb3082ae794c2bffd2330e29b8f0
Vladyslav Levkovich <levkovich.vlad.2004@gmail.com>
2023-12-11 08:40:50 +0200

    fix: remove function clean_data

--- tests/login/conftest.py
+++ tests/login/conftest.py
@@ -4,12 +4,6 @@ from framework.login_page import LoginPage
 from tests.conftest import driver
 
 
-# @pytest.fixture(scope='function', autouse=True)
-# def clear_data():
-#     subprocess.call(['adb', 'shell', 'pm', 'clear', 'com.ajaxsystems'])
-#     yield
-
-
 @pytest.fixture(scope='function')
 def user_login_fixture(driver):
     yield LoginPage(driver)
